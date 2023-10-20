206ff17bb3d422494c959fd240fdfcbcce89201b
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2023-10-20 10:41:05 +0200

    add separate game for SpringCleaning.

---
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet07/presence/SpringCleaningHamsterGame.java
@@ -0,0 +1,24 @@
+package de.unistuttgart.iste.sqa.pse.sheet07.presence;
+
+import de.hamstersimulator.objectsfirst.external.simple.game.SimpleHamsterGame;
+
+/**
+ * This class is used to solve some easy tasks.
+ */
+public class SpringCleaningHamsterGame extends SimpleHamsterGame {
+
+	/**
+	 * Creates a new ArtemisPauleHamsterGame.<br>
+	 * Do not modify!
+	 */
+	public SpringCleaningHamsterGame() {
+		this.loadTerritoryFromResourceFile("/territories/ArtemisPaule.ter");
+		this.displayInNewGameWindow();
+		game.startGame();
+	}
+
+	@Override
+	protected void run() {
+		// Put your code for part A exercise 2 (e) here.
+	}
+}
