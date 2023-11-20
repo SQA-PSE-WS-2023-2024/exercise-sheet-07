59e160b982fad2c2975714a9a526fa10c695c30b
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2023-11-20 15:55:25 +0100

    fix SpringCleaningGameApp.

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet07/presence/SpringCleaningHamsterGameApp.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet07/presence/SpringCleaningHamsterGameApp.java
@@ -3,7 +3,7 @@ package de.unistuttgart.iste.sqa.pse.sheet07.presence;
 public class SpringCleaningHamsterGameApp {
 
 	public static void main(final String[] args) {
-		final ArtemisPauleHamsterGame game = new ArtemisPauleHamsterGame();
+		final SpringCleaningHamsterGame game = new SpringCleaningHamsterGame();
 		game.doRun();
 	}
 }
