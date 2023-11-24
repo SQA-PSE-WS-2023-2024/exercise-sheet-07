8dc30e8eaebb3e5dbf923d209bd3a852a9804bfd
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2023-11-24 10:37:18 +0100

    add helpers to ascertain preconditions

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet07/homework/painter/PainterPauleHamsterGame.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet07/homework/painter/PainterPauleHamsterGame.java
@@ -27,4 +27,84 @@ public class PainterPauleHamsterGame extends SimpleHamsterGame {
 	protected void run() {
 		// insert your code here
 	}
+	
+	
+	
+	
+	////////////////////////////////////////
+	// helpers to ascertain preconditions //
+	////////////////////////////////////////
+	
+	/**
+	 * Calculate paules distance to the next wall. 
+	 * 
+	 * (Only Works, if the territory is quadratic and has no inner walls)
+	 * 
+	 * @return paule's distance to the next wall.
+	 */
+	private int getDistanceToWall() {
+		int size = game.getTerritory().getTerritorySize().getColumnCount();
+		switch (paule.getDirection()){
+		case NORTH: {
+			return paule.getLocation().getRow();			
+		}
+		case EAST: {
+			return size - paule.getLocation().getColumn();
+		}
+		case SOUTH: {
+			return size - paule.getLocation().getRow();
+		}
+		case WEST: {
+			return paule.getLocation().getColumn();
+		}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + paule.getDirection());
+		}		
+	}
+	
+	/** 
+	 * Calculate the number of grains paule currently hold in his mouth.
+	 * 
+	 * @return number of grains currently in paule's mouth.
+	 */
+	private int getPaulesGrainCount() {
+		int grainCounter = 0;
+		/*@
+		@ loop_invariant paule put a grain for each already executed loop iteration. 
+		@ decreasing grains remaining in paules mouth.
+		@*/
+		while(!paule.mouthEmpty()) {
+			paule.putGrain();
+			grainCounter++;
+		}
+		/*@
+		@ loop_invariant paule picked a grain for each already executed loop iteration. 
+		@ decreasing grains remaining on the tile.
+		@*/
+		while(paule.grainAvailable()) {
+			paule.pickGrain();
+		}
+		return grainCounter;
+	}
+	
+	/**
+	 * Calculate the number of grains required to paint the desired spiral on the current territory.
+	 * 
+	 * @return number of grains required to paint the desired spiral on the current territory.
+	 */
+	private int getNumbeOfRequiredGrains() {
+		final int territorySize = game.getTerritory().getTerritorySize().getColumnCount();
+		int lineSize = territorySize - 3;
+		
+		int total = lineSize;
+		/*@
+		@ loop_invariant total is the sum of the previous total plus two times the current linesize + 2
+		@ decreasing lineSize
+		@*/
+		while(lineSize > 0) {
+			total += 2 * lineSize;
+			lineSize -=2;
+		}
+		return total;
+	}
 }
