1a323c4013c41918f8ff76262d348f448ba80dfa
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2023-12-01 08:43:10 +0100

    apply formatter.

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet07/homework/immutable/Person.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet07/homework/immutable/Person.java
@@ -1,30 +1,31 @@
 package de.unistuttgart.iste.sqa.pse.sheet07.homework.immutable;
 
 import java.util.Date;
+
 /**
- * Represents a person with a name and birthdate. 
- * 
+ * Represents a person with a name and birthdate.
+ *
  * @author your name
  */
 public class Person {
-	//@ private instance invariant name != null && name.length() > 0;
-	//@ private instance invariant birthDate != null;
-	
+	// @ private instance invariant name != null && name.length() > 0;
+	// @ private instance invariant birthDate != null;
+
 	public String name;
 	public Date birthDate;
-	
+
 	/*@
-	  @ requires name != null && name.length() > 0;
-	  @ requires birthDate != null;
-	  @ ensures this.name == name;
-	  @ ensures this.birthDate == birthDate;
-	  @*/
+	@ requires name != null && name.length() > 0;
+	@ requires birthDate != null;
+	@ ensures this.name == name;
+	@ ensures this.birthDate == birthDate;
+	@*/
 	/**
 	 * Creates a new person with the given name and birthdate.
-	 * 
+	 *
 	 * @param name Name of the person.
 	 * @param birthDate Birth date of the person.
-	*/
+	 */
 	public Person(final String name, final Date birthDate) {
 		if (name == null || name.length() == 0) {
 			throw new IllegalArgumentException("A person may not have a null or empty name");
@@ -35,7 +36,6 @@ public class Person {
 		this.name = name;
 		this.birthDate = birthDate;
 	}
-	
 
 	/**
 	 * @return This person's name.
@@ -43,7 +43,6 @@ public class Person {
 	public /*@ pure @*/ String getName() {
 		return name;
 	}
-	
 
 	/**
 	 * @return This person's birth date.
@@ -51,11 +50,11 @@ public class Person {
 	public /*@ pure @*/ Date getBirthDate() {
 		return birthDate;
 	}
-	
+
 	/*@
-	  @ requires name != null;
-	  @ ensures this.name == name;
-	  @*/
+	@ requires name != null;
+	@ ensures this.name == name;
+	@*/
 	/**
 	 * Sets this person's name.
 	 * @param name The new name.
@@ -67,14 +66,14 @@ public class Person {
 		}
 		this.name = name;
 	}
-	
+
 	/*@
-	  @ requires birthDate != null;
-	  @ ensures this.birthDate == birthDate;
-	  @*/
+	@ requires birthDate != null;
+	@ ensures this.birthDate == birthDate;
+	@*/
 	/**
 	 * Sets this person's birth date.
-	 * 
+	 *
 	 * @param birthDate The new birth date.
 	 */
 	public void setBirthDate(final Date birthDate) {
@@ -83,5 +82,4 @@ public class Person {
 		}
 		this.birthDate = birthDate;
 	}
-	
 }
